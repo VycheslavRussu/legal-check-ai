c5ee5ea18c863cfe04190446c98d341e8e0a4dc0
Arsen <sedirov07@mail.ru>
2024-05-15 19:39:18 +0500

    Изменены зависимости и Streamlit чат с моделью, добавлена интеграция с yandex-gpt

--- ui.py
+++ ui.py
@@ -1,17 +1,10 @@
 import streamlit as st
-import socket
-import hashlib
 import base64
-import io
-# from models_api.yandex_gpt.yandex-gpt-api.py import UseCase
-from PIL import Image
+# from models_api.yandex_gpt.yandex_gpt_api.py import UseCase
+from models_api.yandex_gpt.yandex_gpt_api import UseCase
 from streamlit import session_state as session
 from docx import Document
-
-
-# Функция хэширования ip-адреса пользователя
-def generate_unique_user_id(ip_address):
-    return hashlib.sha256(ip_address.encode()).hexdigest()
+import json
 
 
 # Функция для чтения содержимого файла формата .txt в текст
@@ -36,83 +29,79 @@ def read_pdf(file):
 
 # Функция для чтения изображения в кодировку base64
 def read_image(file):
-    image = Image.open(file)
-    buffered = io.BytesIO()
-    image.save(buffered, format="PNG")
-    img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
-    return img_str
+    file_contents = file.getvalue()
+    file_base64 = base64.b64encode(file_contents).decode('utf-8')
+    return file_base64
 
 
 # Главная информация страницы
 st.title("Проверка договора на наличие подозрительных пунктов")
 st.header("Загрузка документа")
 
-
-# Проверяка наличия уникального идентификатора пользователя в session_state
-if "user_id" not in session:
-    # Если нет, то создаем новый уникальный идентификатор для пользователя
-    session.user_id = generate_unique_user_id(socket.gethostbyname(socket.gethostname()))
-
 # Проверка существования словаря сообщений для данного user_id в текщей сессии
 if "messages" not in session:
     # Если нет, создаем новую запись с пустым словарем сообщений
-    session.messages = {}
+    session.messages = []
 
 uploaded_file = st.file_uploader(label="Загрузите документ",
                                  type=["png", "jpeg", "jpg", "pdf", "docx"])
-
 if not uploaded_file:
     st.warning("Пожалуйста, загрузите документ!")
     st.stop()
+
 else:
     if uploaded_file.type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
-        session.text = read_docx(uploaded_file)
+        session.doc_text = read_docx(uploaded_file)
+
     elif uploaded_file.type == "text/plain":
-        session.text = read_txt(uploaded_file)
+        session.doc_text = read_txt(uploaded_file)
+
     elif uploaded_file.type == "application/pdf":
-        session.text = None
+        session.doc_text = None
         session.document = read_pdf(uploaded_file)
     elif uploaded_file.type in ["image/png", "image/jpeg", "image/jpg"]:
-        session.text = None
+
+        session.doc_text = None
         session.document = read_image(uploaded_file)
         image_as_bytes = uploaded_file.read()
         st.image(image_as_bytes, use_column_width=True)
 
-    doc_button = st.button("Отправить документ")
-
-if doc_button:
-    if session.text is None:
+    if session.doc_text is None:
         # TODO: отправка документа на модель и получение текста
         # text_doc = model.to_text()
-        session.text = ''
+        session.doc_text = 'Как твои дела?'
 
-    # TODO: отправка текста документа на модель и получение результата
-    # session.model = UseCase()
-    # response_doc = session.model.get_response(session.text)
-    response_doc = ''
-    session.messages[session.user_id] = {'message': ['Отправлен файл...'], 'response': [response_doc]}
-
-    st.write(f"Ответ от модели: {response_doc}")
+    if len(session.messages) == 0:
+        session.messages.append({'role': 'user', 'content': 'Отправка файла...'})
+        session.model = UseCase()
+        response_doc = session.model.execute(session.doc_text)
+        # response_doc = 'Ответ модели на документ'
+        session.messages.append({'role': 'ai', 'content': response_doc})
 
 # После ответа от модели появлется возможность общения
 st.subheader("Обращение к модели")
-user_input = st.text_area("Введите сообщение:", "", max_chars=200)
-text_button = st.button("Отправить сообщение")
+user_input = st.chat_input("Введите сообщение:", max_chars=200)
 
-if text_button and user_input:
-    # TODO: в response ответ модели на user_input
-    # response = session.model.get_response(user_input)
-    response = f'Ответ модели на {user_input}'
+for message in session.messages:
+    with st.chat_message(message['role']):
+        st.markdown(message['content'])
 
-    # Если запись для данного user_id уже существует, добавляем сообщение в список
-    if session.user_id in session.messages:
-        session.messages[session.user_id]['message'].append(user_input)
-        session.messages[session.user_id]['response'].append(response)
-    # Если запись для данного user_id еще не существует, создаем новую запись
-    else:
-        session.messages[session.user_id] = {'message': [user_input], 'response': [response]}
+# После сообщения пользователя
+if user_input:
+    # Показывает сообщение пользователя
+    with st.chat_message('user'):
+        st.markdown(user_input)
 
-    # Отображаем все сообщения для данного пользователя
-    for i in range(len(session.messages[session.user_id]['message'])):
-        st.write(f"Вы: {session.messages[session.user_id]['message'][i]}")
-        st.write(f"Ответ: {session.messages[session.user_id]['response'][i]}")
+    # Добавляет сообщение в историю сообщений
+    session.messages.append({'role': 'user', 'content': user_input})
+
+    # Ответ модели на запрос
+    response = session.model.execute(user_input)
+    # response = f'Ответ модели на {user_input}'
+
+    # Показывает ответ модели
+    with st.chat_message('ai'):
+        st.markdown(response)
+
+    # Добавляет ответ модели в историю сообщений
+    session.messages.append({'role': 'ai', 'content': response})
